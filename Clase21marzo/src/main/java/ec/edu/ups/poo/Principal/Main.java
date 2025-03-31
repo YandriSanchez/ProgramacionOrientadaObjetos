01c5b0e3245f148185fe84a170f0d8707dd44360
mathias <canazcol@est.ups.edu.ec>
2025-03-31 11:42:05 -0500

    FIX(UPS-002):Comentario master

--- Clase21marzo/src/main/java/ec/edu/ups/poo/Principal/Main.java
+++ Clase21marzo/src/main/java/ec/edu/ups/poo/Principal/Main.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("comentario master");
 
         Scanner scanner = new Scanner(System.in);
         Main main = new Main();
