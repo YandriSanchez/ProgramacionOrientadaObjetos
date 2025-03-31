f73c411ca95c57c55388b66aa311368503fef691
mathias <canazcol@est.ups.edu.ec>
2025-03-30 23:21:03 -0500

    LOG(UPS-002): Añadir primer mensaje informativo commit de Mathias Añazco

--- Clase21marzo/src/main/java/ec/edu/ups/poo/Principal/Main.java
+++ Clase21marzo/src/main/java/ec/edu/ups/poo/Principal/Main.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Primer commit");
 
         Scanner scanner = new Scanner(System.in);
         Main main = new Main();
